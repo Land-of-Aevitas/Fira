5ee7693fe8b9293513882fddd8ed610220200520
OK_Diamond <ok.thekirks@gmail.com>
2024-05-04 04:55:42 +0100

    Add functionality to read test.fira

---
+++ README.md
@@ -0,0 +1,4 @@
+# Fiṟa
+
+Old version:
+https://docs.google.com/spreadsheets/d/13KDITzV5F0D-_dOVp5ZiHeFLWGx1e0V6SV9oRrzeODw/edit#gid=1235548948
--- fira_script.py
+++ fira_script.py
@@ -1,9 +1,8 @@
 '''
 The main reader for 'FiraScript'.
 '''
-import sql_lib as sql
-
-FILE_LOC = "./Fira"
+import re
+from zemia import sql
 
 class RootWordTable(sql.Table):
     '''Initialises the WordTable, which stores all words that have been defined so far, along with the formulas to define them.'''
@@ -25,14 +24,36 @@ class WordTable(sql.Table):
 
 def read_file(filename: str) -> list[str]:
     '''Reads the file and returns its content as a list of strings.'''
-    with open(f"{FILE_LOC}/{filename}", "r", encoding="utf-8") as f:
-        content = f.read().splitlines()
-
-    for line in content:
-        print(line)
-
-read_file("test.fira")
+    with open(filename, "r", encoding="utf-8") as f:
+        return f.read().splitlines()
 
 
-sql_connection = sql.connect(f"{FILE_LOC}/fira.db")
-root_word_table = RootWordTable(sql_connection)
+
+def main() -> None:
+    '''Main function.'''
+    #sql_connection = sql.connect(f"{FILE_LOC}/fira.db")
+    #root_word_table = RootWordTable(sql_connection)
+
+    user_inp = read_file("test.fira")
+    for line in user_inp:
+        command_list = line.split(" ")
+        for i, command in enumerate(command_list):
+            if command == "": # Skip empty commands
+                continue
+            if re.search('^"', command) is not None and re.search('"$', command) is None: # Check if the command is the start of a multi-word String
+                merge_end = -1
+                # Find the end of the string
+                for j in range(i+1, len(command_list)):
+                    if re.search('"$', command_list[j]):
+                        merge_end = j
+                        break
+                # Merge the string
+                for k in range(i+1, merge_end+1):
+                    command_list[i] += f" {command_list[k]}"
+                # Remove other parts from the list
+                for _ in range(merge_end-i):
+                    command_list.pop(i+1)
+            print(command, end=" ")
+        print()
+
+main()
